f300e9c49baa166ad221ba0e4d5a075c6ec6945a
AmirAliLoghmani80 <143129413+AmirAliLoghmani80@users.noreply.github.com>
2025-02-02 00:54:40 +0330

    Update loader.py

--- loader.py
+++ loader.py
@@ -3,26 +3,22 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets
 
-img_size=64
+img_size = 64
+
 transform = torchvision.transforms.Compose([
     torchvision.transforms.Resize((img_size, img_size)),
     torchvision.transforms.RandomHorizontalFlip(),
-    torchvision.transforms.ToTensor(),])
-#torchvision.transforms.Normalize([ 0.5, 0.5, 0.5 ], [ 0.5, 0.5, 0.5 ])])
-'''invTrans = torchvision.transforms.Compose([ torchvision.transforms.Normalize(mean = [ 0., 0., 0. ],
-                                                     std = [ 1/0.5, 1/0.5, 1/0.5 ]),
-                                torchvision.transforms.Normalize(mean = [ -0.5, -0.5, -0.5],
-                                                     std = [ 1., 1., 1. ]),
-                               ])'''
-
+    torchvision.transforms.ToTensor(),
+])
 
 def train_loader_fn(batch_size):
+    # Change this path to the location of your CelebA dataset on Kaggle
+    celeba_path = "/kaggle/input/celeba-dataset/img_align_celeba"
 
-    # train_dataset = torchvision.datasets.ImageFolder(root="~/torch_datasets/img_align_celeba", transform=transform)
-    # train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    celeba_data = datasets.CelebA(root="/content/celeba", split="train", transform=transform)
+    # Using CelebA dataset from torchvision.datasets
+    celeba_data = datasets.CelebA(root=celeba_path, split="train", transform=transform)
 
-    # You can access the data with a DataLoader
-
+    # Create the DataLoader for training
     train_loader = DataLoader(celeba_data, batch_size=batch_size, shuffle=True)
+
     return train_loader
